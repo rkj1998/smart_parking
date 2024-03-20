bfe2e00ff78b8444b4bac32ce0351a82923f7187
Rahul Jha <62897573+rkj1998@users.noreply.github.com>
2024-03-19 18:46:01 -0600

    Parking Locations
    Integrated Google Maps API to display the parking locations around me

--- lib/Widget/recents.dart
+++ lib/Widget/recents.dart
@@ -1,89 +1,118 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_parking/Widget/Rating.dart';
-import 'package:smart_parking/helper/fake_data.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:smart_parking/const.dart';
 
-
-class Recents extends StatefulWidget{
-
+class Recents extends StatefulWidget {
   @override
-  State createState() => _RecentState();
+  State createState() => _RecentsState();
 }
 
-class _RecentState extends State<Recents> {
+class _RecentsState extends State<Recents> {
+  List<dynamic> nearbyParkingList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchNearbyParking();
+  }
+
+  Future<void> _fetchNearbyParking() async {
+    await _requestLocationPermission(); // Request location permission
+    Position position = await _getCurrentLocation();
+    if (kDebugMode) {
+      print(position);
+    }
+
+    const String placesEndpoint =
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+    final String location =
+        '${position.latitude},${position.longitude}';
+    const String radius = '1000'; // Radius in meters, adjust as needed
+    const String type = 'parking';
+    const String keyword = 'parking';
+
+    final String request =
+        '$placesEndpoint?location=$location&radius=$radius&type=$type&keyword=$keyword&key=$apiKey';
+
+    final http.Response response = await http.get(Uri.parse(request));
+
+    if (response.statusCode == 200) {
+      final dynamic data = json.decode(response.body);
+      setState(() {
+        nearbyParkingList = data['results'];
+      });
+    } else {
+      throw Exception('Failed to load nearby parking');
+    }
+  }
+
+  Future<void> _requestLocationPermission() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled, throw an error or handle it accordingly
+      throw Exception('Location services are disabled.');
+    }
+
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission != LocationPermission.always &&
+        permission != LocationPermission.whileInUse) {
+      // Permission not granted, throw an error or handle it accordingly
+      throw Exception('Location permission is not granted.');
+    }
+  }
+
+  Future<Position> _getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    return position;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: ListView.builder(
-          itemCount: recentList.length,
-          physics: const BouncingScrollPhysics(),
-          itemBuilder: (context,index){
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 14.0,horizontal: 24),
-              child: Card(
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                  elevation: 6,
-                  color: Colors.black.withOpacity(0.8),
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 14),
-                    child: Column(
-                      children: <Widget>[
-                        ListTile(
-                          title: Text(
-                            '${recentList[index].title}',
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 17.5,
-                                letterSpacing: 0.2
-                            ),
-                          ),
-                          subtitle: Text(
-                            '${recentList[index].subtitle}',
-                            style: const TextStyle(
-                                color: Colors.white70,
-                                fontWeight: FontWeight.w400,
-                                fontSize: 13.5,
-                                letterSpacing: 0.1
-
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(left: 12.0),
-                                child: StarRating(
-                                  size: 20,
-                                  color: Colors.yellow,
-                                  rating: recentList[index].rating, onRatingChanged: (double rating) {  },
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 12.0),
-                                child: Text(
-                                  '\u20B9 ${recentList[index].price}',
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w800,
-                                      fontSize: 17
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  )
+        itemCount: nearbyParkingList.length,
+        itemBuilder: (context, index) {
+          final parking = nearbyParkingList[index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
               ),
-            );
-          }
+              color: Colors.grey[900],
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      parking['name'],
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      parking['vicinity'],
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
-
 }
